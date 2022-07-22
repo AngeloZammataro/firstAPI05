45743368cdb0ff41532b2c36739b9c5b3194568f
Angelo Zammataro <juba.kem@gmail.com>
2022-07-22 16:16:21 +0200

    First Commit: Primi passi - First API - 05

---
+++ src/main/java/it2/develhope/firstAPI05/DTO/CarDTO.java
@@ -0,0 +1,21 @@
+package it2.develhope.firstAPI05.DTO;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+
+import javax.validation.constraints.NotBlank;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@ToString
+public class CarDTO {
+
+    @NotBlank(message = "Mandatory")
+    private String id;
+    @NotBlank(message = "Mandatory")
+    private String modelName;
+    private double price;
+}
